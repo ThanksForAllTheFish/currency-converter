3d5daf6e3687ba9b058c3e812fe263399186f1b1
Marco Davi <marco.davi@localsearch.ch>
2015-11-28 12:43:14 +0100

    Service return a json

--- src/main/java/org/t4atf/currency/converter/controllers/CurrencyConverter.java
+++ src/main/java/org/t4atf/currency/converter/controllers/CurrencyConverter.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import javax.annotation.Resource;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,20 +15,37 @@ import org.t4atf.currency.converter.exceptions.UnknownCurrenciesException;
 import org.t4atf.currency.converter.rate.RateSet;
 import org.t4atf.currency.converter.rate.provider.RateProvider;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/")
 public class CurrencyConverter {
 
 	@Resource private RateProvider rateProvider;
 
-	@RequestMapping(value = "rate", method = RequestMethod.GET)
-	public BigDecimal getConversionRate(
+	@RequestMapping(value = "rate", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Response getConversionRate(
 			@RequestParam("from") Currency from,
 			@RequestParam("to") Currency to,
 			@RequestParam("amount") BigDecimal amount) {
 
 		RateSet rates = rateProvider.getRates();
 		Optional<BigDecimal> converted = rates.convert(from, to, amount);
-		return converted.orElseThrow( () -> new UnknownCurrenciesException(from, to));
+		return converted.map( (result) -> new Response(from.getCurrencyCode(), to.getCurrencyCode(), amount.toString(), result.toString()) )
+			.orElseThrow( () -> new UnknownCurrenciesException(from, to));
+	}
+
+	@RequiredArgsConstructor
+	private static class Response {
+		@JsonProperty("from")
+		private final String from;
+		@JsonProperty("to")
+		private final String to;
+		@JsonProperty("amount")
+		private final String amount;
+		@JsonProperty("result")
+		private final String result;
 	}
 }
--- src/test/java/org/t4atf/currency/converter/SpringBootApplicationTest.java
+++ src/test/java/org/t4atf/currency/converter/SpringBootApplicationTest.java
@@ -8,11 +8,11 @@ import static org.springframework.test.web.client.response.MockRestResponseCreat
 import static org.t4atf.currency.converter.utils.FileUtils.readFileAsString;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.net.URISyntaxException;
 
 import javax.annotation.Resource;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -24,12 +24,19 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.rules.SpringClassRule;
 import org.springframework.test.context.junit4.rules.SpringMethodRule;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
 import org.t4atf.currency.converter.rate.provider.ECBRateProvider;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @SpringApplicationConfiguration(SpringBootApplication.class)
 @WebIntegrationTest({"server.port=9000"})
@@ -45,24 +52,58 @@ public class SpringBootApplicationTest {
 	@Resource
 	private RestTemplate restTemplate;
 
-	private RestTemplate client = new TestRestTemplate();
+	private RestTemplate client;
+	private MockRestServiceServer mockServer;
 
 	@Before
 	public void init() throws IOException, URISyntaxException {
-		MockRestServiceServer.createServer(restTemplate)
+		client = new TestRestTemplate();
+		client.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+
+		mockServer = MockRestServiceServer.createServer(restTemplate);
+		mockServer
 			.expect(requestTo(ECBRateProvider.RATES_LOCATION)).andExpect(method(HttpMethod.GET))
 			.andRespond(withSuccess(readFileAsString("fakeRates.xml"), MediaType.TEXT_XML));
 	}
 
 	@Test
 	public void validConversion() {
-		BigDecimal rate = client.getForObject("http://localhost:9000/rate?from=EUR&to=USD&amount=100", BigDecimal.class);
-		assertThat(rate, equalTo(new BigDecimal("106.12")));
+		Response rate = client.getForObject("http://localhost:9000/rate?from=EUR&to=USD&amount=100", Response.class);
+		assertThat(rate, equalTo(new Response("EUR", "USD", "100", "106.12")));
 	}
 
 	@Test
 	public void unknownCurrencyCombination() {
 		ResponseEntity<String> response = client.getForEntity("http://localhost:9000/rate?from=EUR&to=GBP&amount=100", String.class);
 		assertThat(response.getStatusCode(), equalTo(HttpStatus.NOT_FOUND));
+	}
+
+	@After
+	public void close() {
+	    mockServer.verify();
+	}
+
+	@ToString
+	@EqualsAndHashCode
+	private static class Response {
+		private final String from;
+		private final String to;
+		private final String amount;
+		private final String result;
+
+		private Response(String from, String to, String amount, String result) {
+			this.from = from;
+			this.to = to;
+			this.amount = amount;
+			this.result = result;
+		}
+
+		@JsonCreator
+		public static Response newResponse(@JsonProperty("from") String from,
+										   @JsonProperty("to") String to,
+										   @JsonProperty("amount") String amount,
+										   @JsonProperty("result") String result) {
+			return new Response(from, to, amount, result);
+		}
 	}
 }
